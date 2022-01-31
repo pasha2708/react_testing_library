6739a6c52effb647ccd373c72e00638dc6b59c10
pasha2708 <pasha2708@gmail.com>
2022-01-31 21:49:49 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const URL = "http://hn.algolia.com/api/v1/search";
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleFetch = async () => {
+    try {
+      const result = await axios.get(`${URL}?query=React`);
+      setNews(result.data.hits);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={handleFetch}>
+        Fetch News
+      </button>
+
+      {error && <span>Something went wrong ...</span>}
+
+      <ul>
+        {news.map(({ objectID, url, title }) => (
+          <li key={objectID}>
+            <a href={url}>{title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
